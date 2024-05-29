6e627129fb22f4231034ca22ddf163a0fab49057
usamakhan5515690 <uk5515690@gmail.com>
2024-05-29 15:56:30 +0500

    first commit

---
+++ csharp/project1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("kse ho piare Bhai");
+Console.WriteLine("Hows your first dat at class of Game Development.");
